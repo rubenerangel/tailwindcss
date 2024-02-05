0c4661785cd6ef91babc9c6af73d6fb52a213d51
Rubén Rangel <rubenrang@gmail.com>
2024-02-05 18:54:44 -0500

    Dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // darkMode: ["class", "[data-theme=dark]"],
+  darkMode: "class",
   content: ["./*.html"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
