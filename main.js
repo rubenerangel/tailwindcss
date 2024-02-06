8d03b94582a0e820a3914bdf26d6744f95f4e629
Rubén Rangel <rubenrang@gmail.com>
2024-02-06 14:01:41 -0500

    Sincronize Dark mode with SO

--- main.js
+++ main.js
@@ -3,6 +3,28 @@ const html = document.documentElement;
 const sunIcon = document.getElementById("sun-icon");
 const moonIcon = document.getElementById("moon-icon");
 
+const matchMedia = window.matchMedia("(prefers-color-scheme: dark)");
+
+const systemDarkMode = matchMedia.matches;
+
+updateTheme(systemDarkMode);
+
+matchMedia.addEventListener("change", ({ matches }) => {
+  updateTheme(matches);
+});
+
+function updateTheme(darkMode) {
+  if (darkMode) {
+    html.classList.add("dark");
+    sunIcon.classList.add("hidden");
+    moonIcon.classList.remove("hidden");
+  } else {
+    html.classList.remove("dark");
+    sunIcon.classList.remove("hidden");
+    moonIcon.classList.add("hidden");
+  }
+}
+
 toggleThemeButton.addEventListener("click", () => {
   //   toggle dark class
   html.classList.toggle("dark");
